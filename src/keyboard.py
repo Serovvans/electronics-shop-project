eac0dcef920cb47336f27cb9352f4f21f7db2d5b
Serov Ivan <serovvans@yandex.ru>
2023-08-16 20:44:26 +0300

    add keyboard class and tests

---
+++ src/keyboard.py
@@ -0,0 +1,29 @@
+from src.item import Item
+
+
+class MixinChange:
+    """Класс добавления функционала смены языка"""
+    def __init__(self, name: str, price: float, quantity: int, language: str):
+        """Инициализация данных через класс Item и добавление аттрибута языка"""
+        super().__init__(name, price, quantity)
+        self.__language = language
+
+    @property
+    def language(self):
+        """Getter для языка раскладки"""
+        return self.__language
+
+    def change_lang(self):
+        """Метод смены языка раскладки"""
+        if self.__language == "RU":
+            self.__language = "EN"
+        else:
+            self.__language = "RU"
+        return self
+
+
+class Keyboard(MixinChange, Item):
+    """Класс представления для товара клавиатуры"""
+    def __init__(self,  name: str, price: float, quantity: int, language: str = "EN"):
+        """Инициализация объекта клавиатуры через класс MixinChange"""
+        super().__init__(name, price, quantity, language)
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.item import Item
 from src.phone import Phone
+from src.keyboard import Keyboard
 
 
 @pytest.fixture(scope="module")
@@ -20,3 +21,9 @@ def item_laptop() -> Item:
 def phone1() -> Phone:
     """Фикстура iPhone класса Phone"""
     return Phone("iPhone 14", 120_000, 5, 2)
+
+
+@pytest.fixture(scope="session")
+def keyboard() -> Keyboard:
+    """Фикстура Keyboard класса Keyboard"""
+    return Keyboard('Dark Project KD87A', 9600, 5)
